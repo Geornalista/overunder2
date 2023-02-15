f7c6fa6cfd3b3ff9f0ef94ce086e7e8a9b914d9f
Geornalista <83824843+Geornalista@users.noreply.github.com>
2023-02-15 09:59:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ st.sidebar.header(
 
     """
 )
+
+with open('style.css') as f:
+	st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
+  
 tab1,tab2 = st.tabs([
                   "📊 ESTATÍSTICAS",
                   "🥅 FAVORITOS"])
